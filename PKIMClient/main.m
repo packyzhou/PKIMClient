2398c2d90d71d5f4c2c44b7d408957e3c141090f
packy周 <packyzhou@icloud.com>
2015-09-30 10:53:41 +0800

    创建微信客户端项目
    add we chat

---
+++ PKIMClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PKIMClient
+//
+//  Created by 周经伟 on 15/9/30.
+//  Copyright (c) 2015年 packy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
